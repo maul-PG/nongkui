4c3976e2567735983317ce94a399b99b430ddc74
maul-PG <124240138@student.upnyk.ac.id>
2026-06-26 15:37:01 +0700

    chore: integrate Vercel Speed Insights and trigger preview deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import { Inter, Outfit } from 'next/font/google';
 import '../styles/globals.css';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ['latin'],
@@ -20,6 +21,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <main className={`${inter.variable} ${outfit.variable} font-sans`}>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </main>
   );
 }
